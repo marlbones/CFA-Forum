314b62d5316351e9043bd5a3aafc32cbc600d0e2
marlbones <mdimaanods@gmail.com>
2017-03-31 17:11:14 +1100

    added rolify action in User method to set user role as member upon pressing create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:member) if self.roles.blank?
+  end
 end
